2718f8fc903531931bedeb469fa47b9bb71adbee
Nikki Strømsnes <hello@tinybits.dk>
2022-08-22 09:35:44 +0200

    Add small JSDoc example

--- src/DataTable/Pagination.jsx
+++ src/DataTable/Pagination.jsx
@@ -3,6 +3,12 @@ import PropTypes from 'prop-types'
 import Page from './Page.jsx'
 import { ITEMS_PER_PAGE } from '../constants.js'
 
+/**
+ * Calculates how many pages to display in a list
+ * @param {number} rowsFound - The total amount of rows (items)
+ * @param {number} rowsPerPage - How many rows /items are on a single page
+ * @returns {number} - The total amount of pages to display
+ */
 export const calculatePageCount = (rowsFound, rowsPerPage = ITEMS_PER_PAGE) =>
   rowsPerPage ? Math.ceil(rowsFound / rowsPerPage) : 0
 
